f7203a228e2136b7d72e14c31a34b848addca840
QuimbanoPrime <qaz027@pm.me>
2025-12-05 15:21:32 -0800

    adding changes to main.py from Ch2 L3 - FPS

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -20,6 +23,9 @@ def main():
 
         screen.fill('black')
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        #print(f"Delta time: {dt:.4f} seconds")
 
 if __name__ == "__main__":
     main()
