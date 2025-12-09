f0e79edbee602739dbd3869ae8d201d34cd01542
QuimbanoPrime <qaz027@pm.me>
2025-12-08 21:17:08 -0800

    creating move method for player and updating constants with player speed

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
-from circleshape import circleshape
-from constants import LINE_WIDTH, PLAYER_RADIUS
+from circleshape import CircleShape
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
-class Player(circleshape.CircleShape):
+class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation=0
@@ -18,3 +18,29 @@ class Player(circleshape.CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # ?
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            # ?
+            self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
