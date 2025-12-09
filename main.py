bda29900e0c6b95c15c36b1dd8ed90c50bc8a36d
QuimbanoPrime <qaz027@pm.me>
2025-12-08 22:21:28 -0800

    adjusting main to use pygame groups

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -24,8 +29,11 @@ def main():
                 return
 
         screen.fill('black')
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        #player.update(dt)
+        for items in drawable:
+            items.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()
         
 
